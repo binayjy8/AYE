65a44f991b622c7e67e58f9edb6a7de14c4ee1be
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-29 08:02:40 +0530

    file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
 
+app.get("/", (req, res) => {
+    res.send("Hi i'm root");
+});
+
 app.listen(8080, () => {
     console.log("server is listening to the port 8080");
-})
+});

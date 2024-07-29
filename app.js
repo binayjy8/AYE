862fa2fc73d72236da161f5ff6375d9dc39e25e2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-29 08:07:26 +0530

    file

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
 app.get("/", (req, res) => {
     res.send("Hi i'm root");
 });

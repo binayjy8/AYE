c63b9b109fa74de5b7abc6eea03fa459f013b80f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-06 18:38:47 +0530

    data file

--- init/data.js
+++ init/data.js
@@ -1,5 +1,7 @@
 const sampleListing = [
     {
         title: "Cozy Beachfront Cottage",
+        description: "The sea is a vast expanse of salty water covering most of our planet.",
+        
     }
 ]

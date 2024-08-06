063bae4af9e394f5f925dfb4c2f9ac58e20c001e
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-06 18:43:14 +0530

    code

--- init/data.js
+++ init/data.js
@@ -2,6 +2,6 @@ const sampleListing = [
     {
         title: "Cozy Beachfront Cottage",
         description: "The sea is a vast expanse of salty water covering most of our planet.",
-        
+        image: "",
     }
 ]

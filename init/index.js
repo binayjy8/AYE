4090e849eba20ace0afcdd3d562f41b45b8a71fb
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-06 20:06:34 +0530

    code push

--- init/index.js
+++ init/index.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
 const data = require("./data.js");
 const Listing = require("./models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};

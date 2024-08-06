d932c8aa0f87e4d4418bd410bd586680784ee30d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-06 18:49:07 +0530

    code

--- init/data.js
+++ init/data.js
@@ -9,5 +9,10 @@ const sampleListing = [
     },
     {
         title: "Modern Loft In Downtown",
+        description: " It's home to countless creatures, from tiny plankton to giant whales.",
+        image: "https://images.unsplash.com/photo-1688553283291-7c0a8c2f7a85?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price: 3100,
+        location: "West Indies",
+        country: "West Indies",
     }
 ]

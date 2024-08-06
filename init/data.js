cdd9f47157870ef8daea5f3de5a57a19ba334e4a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-06 18:46:05 +0530

    code

--- init/data.js
+++ init/data.js
@@ -2,6 +2,9 @@ const sampleListing = [
     {
         title: "Cozy Beachfront Cottage",
         description: "The sea is a vast expanse of salty water covering most of our planet.",
-        image: "",
+        image: "https://images.unsplash.com/photo-1722265733876-45bd4dd9f625?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price: 2100,
+        location: "maibu",
+        country: "United States",
     }
 ]

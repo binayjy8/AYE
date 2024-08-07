db8093603c17ed4f7e754e8026a77739c625546b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-07 19:19:29 +0530

    code push

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const data = require("./data.js");
+const initDB = require("./data.js");
 const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";

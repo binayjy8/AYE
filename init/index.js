d8fd19187daf9a52c505c0f131ee38b8d3834569
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-07 19:22:55 +0530

    code push

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const initDB = require("./data.js");
+const initData = require("./data.js");
 const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
@@ -18,4 +18,5 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    await Listing.insertMany(initData)
 }

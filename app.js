b6788a55d25e59a94411c8a2e84c400c50da22bf
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-11 10:20:08 +0530

    show side

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.get("/listings", async (req, res) => {
 app.get("/listings/:id", async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
 });
 
 app.listen(8080, () => {

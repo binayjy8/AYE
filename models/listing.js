04d1bba285a873cef0fa05a2807d4fe293171421
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-11 10:26:17 +0530

    show side

--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,7 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    descrition: String,
+    description: String,
     image: {
         type: String,
         default: "https://images.unsplash.com/photo-1722648404090-2179fba1b4b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",

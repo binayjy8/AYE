aff4c753659050e114b37974a00732c1c218f074
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-19 09:47:36 +0530

    backend route

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.get("/listings/:id")
+app.get("/listings/:id", async (req, res))
 
 app.listen(8080, () => {
     console.log("server is listening to the port 8080");

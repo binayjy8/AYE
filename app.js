ad57bf97682c8932691719c6bc07856dc6bf8807
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-22 19:29:00 +0530

    backend route

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 //Update Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    res.render("");
+    res.render("listings");
 });
 
 app.listen(8080, () => {

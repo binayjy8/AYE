4a2e26072d7ee3c88f335d74142dc45d9c1f2500
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-23 18:31:52 +0530

    backend code

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 //Update Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    await
+    await Listing.findByIdAndUpdate
 });
 
 app.listen(8080, () => {

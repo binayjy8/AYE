a4acc95fc46d8c2ec31172e3cbc8f28a30540ed9
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-24 09:38:07 +0530

    backend code

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.get("/listings/:id", async (req, res) => {
+app.put("/listings/:id", async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings${id}`);

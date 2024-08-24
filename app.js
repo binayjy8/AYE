2961262af6c3656b05407c25fc8754337a5adda6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-24 09:41:42 +0530

    backend code

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.put("/listings/:id", async (req, res) => {
 
 //Delete Route
 app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
 });
 

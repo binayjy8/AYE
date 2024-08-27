134bf15d09aa4951af32712cefe2044064c59629
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-08-27 15:17:24 +0530

    listing route

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true}));
+app.use()
 
 app.get("/", (req, res) => {
     res.send("Hi i'm root");

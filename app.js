93aa299e4f036586ec3dc6e4368a2c0faa3726f7
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-12 23:23:27 +0530

    middleware

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
     let { status = 500, message = "SOME ERROR--"} = err;
 });
 app.use((err, req, res, next) => {
-    let { status = 500, message = "SO"} = err;
+    let { status = 500, message = "SOME"} = err;
 });
 //Index Route
 app.get("/listings", async (req, res) => {

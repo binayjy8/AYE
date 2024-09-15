e9aacf469139b521752fc5b37364cc6be77b5b0c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-15 15:46:53 +0530

    error handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
 

38282dd6040dddb3149b83a9be299f1395303378
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-16 21:47:00 +0530

    add middleware

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode=500, message="something went wrong!"} = err;
-    res.statusrender("error.ejs", {message});
+    res.status(statusCode).render("error.ejs", {message});
     //res.status(statusCode).send(message);
 });
 

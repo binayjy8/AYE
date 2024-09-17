9ad26830a8a0fcb59723c082d93e91fd9c4a1967
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-17 22:37:15 +0530

    add middleware

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 app.use((err, req, res, next) => {
     let {statusCode, message} = err;
-    res.status(statusCode).render({mess});
+    res.status(statusCode).render({message});
 });
 
 app.all("*", (req, res, next) => {

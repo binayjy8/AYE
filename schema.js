962a7e23a74e66bb6ec9b98dd360832a4f7d8f66
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-18 20:24:51 +0530

    schema validate

--- schema.js
+++ schema.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const listings = Joi.object({
     listing : Joi.object({
         title : Joi.string().required(),
-        description : Joi.string().required()
+        description : Joi.string().required(),
+        location
     }).required()
 });

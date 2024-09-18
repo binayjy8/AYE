5d1e577f1203271ed7aa7582dd711fe77a115812
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-18 20:29:59 +0530

    schema validate

--- schema.js
+++ schema.js
@@ -7,5 +7,6 @@ const listings = Joi.object({
         location : Joi.string().required(),
         country : Joi.string().required(),
         price : Joi.number().required().min(0),
+        image : Joi.string().allow("", null)
     }).required()
 });

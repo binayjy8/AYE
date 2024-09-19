9e790a6f1ea0f6ed22772088b589a56f5f85b062
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-19 22:06:52 +0530

    schema validate

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect("/listings");
 }));
 
-app.use
+app.use();
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "page not found!"));

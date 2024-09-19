c9b4539b0fbe06aad5a6541bef72ab6c4055795f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-19 22:12:07 +0530

    schema validate

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 app.use("/kl", (req, res, next) => {
-    next(new ExpressError(500, "rout"));
+    next(new ExpressError(500, "route"));
 });
 
 app.all("*", (req, res, next) => {

7c8c57dc6647b4dbdacc9e0d035b7be50a5788f4
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-20 21:06:13 +0530

    reviews

--- models/review.js
+++ models/review.js
@@ -1,9 +1,10 @@
+const { number } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     comments: String,
     reting: {
-        type
+        type: Number,
     }
 });

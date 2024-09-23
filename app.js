d024f5c4b38e65bca275550a41cd154f65835101
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-23 21:23:18 +0530

    reviews

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
 

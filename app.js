63d48ff6bc74a4b7057b5d0c33fd5bfbdc902ba5
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-25 11:04:19 +0530

    route

--- app.js
+++ app.js
@@ -35,6 +35,20 @@ app.get("/", (req, res) => {
     res.send("Hi i'm root");
 });
 
+//Review Route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.reviews);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     if(error) {
--- routes/listing.js
+++ routes/listing.js
@@ -49,15 +49,3 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect("/listings");
 }));
 
-//Review Route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.reviews);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));

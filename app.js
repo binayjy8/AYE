755645073ef7040126f31950c5a8a57d17c6f2d5
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-25 11:07:07 +0530

    route

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
+const listing = require("./routes/listing.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/traveling";
 
 main()

eceb771e5fc36e09a7d555fa90d776b560b3d7c0
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-27 20:07:13 +0530

    route

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/listings", listings);
-app.use("/listings/:id/reviews", rev);
+app.use("/listings/:id/reviews", revi);
 
 
 const validateListing = (req, res, next) => {

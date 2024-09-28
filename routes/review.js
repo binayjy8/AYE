7f147278c2630aac6cba42cf279e4ae3a29aca7f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-28 20:44:17 +0530

    route

--- routes/review.js
+++ routes/review.js
@@ -18,7 +18,7 @@ const validateReview = (req, res, next) => {
 
 //Review Route
 router.post("/",validateReview,wrapAsync(async (req, res) => {
-    console.log
+    console.log();
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.reviews);
 

09dba9bcf96a140d54c4f29c1a33a1c2a28df186
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-09-28 20:48:28 +0530

    route

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router({ mergePara});
+const router = express.Router({ mergeParams});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { reviewSchema } = require("../schema.js");

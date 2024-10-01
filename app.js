509b710ddcde1c4498818638a63edd50a3a46ac2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-01 21:44:26 +0530

    route

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
     res.send("Hi i'm root");
 });
 
-app.delete("/delete",(re) );
+app.delete("/delete",(req, r) );
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);

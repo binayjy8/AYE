96e1564b7ccf6b42b0e21c5b8a4dde3e99e185f1
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-03 18:40:38 +0530

    route

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.all("*", (req, res, next) => {
     next(new ExpressError(404, "page not found!"));
 });
 
-app.
+app.u
 
 app.use((err, req, res, next) => {
     let {statusCode=500, message="something went wrong!"} = err;

92daac071c128378b2da71853fc0ed3cd3e1fa4c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-04 21:12:59 +0530

    route

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", {message});
 });
 
-app.get((re));
+app.get((res));
 
 app.listen(8080, () => {
     console.log("server is listening to the port 8080");

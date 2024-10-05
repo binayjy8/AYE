1cb640ef4b915b50f487b52f2a48cf70c9e46ebc
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-05 20:06:28 +0530

    route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const session = requi
+const session = require
 
 
 const listings = require("./routes/listing.js");

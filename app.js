d75e01af26f80c8eee26edf6fcf041dfaebb159f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-06 18:28:09 +0530

    middleware

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const flash = require("connect");
+const flash = require("connect-fl");
 
 
 const listings = require("./routes/listing.js");

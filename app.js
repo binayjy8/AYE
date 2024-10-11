d93aba5ff59a6ed39ec610683eeea6991418072c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-11 21:47:44 +0530

    code push

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "page not found! please look oswsome the some error some of the error"));
+    next(new ExpressError(404, "page not found! please look oswsome the i some error some of the error"));
 });
 
 

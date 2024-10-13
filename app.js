ae7f0020e02001f8325417ff2a192ec5bfffb0b6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-13 19:01:34 +0530

    code push

--- app.js
+++ app.js
@@ -57,8 +57,6 @@ app.all("*", (req, res, next) => {
     next(new ExpressError(404, "page not found !"));
 });
 
-app.use();
-
 app.use((err, req, res, next) => {
     let {statusCode=500, message="something went wrong!"} = err;
     res.status(statusCode).render("error.ejs", {message});

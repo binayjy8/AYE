44804e7cda1e054bad8667257182d5522c17f9df
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-13 18:54:44 +0530

    code push

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.all("*", (req, res, next) => {
 
 
 app.use((err, req, res, next) => {
-    let {statusCode=500, message="something went so wrong!"} = err;
+    let {statusCode=500, message="something went som wrong!"} = err;
     res.status(statusCode).render("error.ejs", {message});
 });
 
